1d05c7a58617d94716d8d42d0602c70cafe98449
Justin Miller <jayman1294@gmail.com>
2019-11-11 11:46:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,9 @@ def artist=(artist_obj)
 end
 
 def self.new_by_filename(file)
-  
+  song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
 end
 
 def self.all
